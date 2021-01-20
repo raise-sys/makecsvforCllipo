d3b22c0587b8c5b6fe1a66d7f67153c8fba3dfca
REI <raise.sys@gmail.com>
2021-01-20 20:29:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -185,7 +185,7 @@ const file_load = () => {
 
     // Excel出力されたCSVの最終行の空行を無視
     // Excel出力されたセル中の改行は\n、行末の改行は\r\n(macは行末\nかも...)
-    csv = csv.replace('"', ' ');
+    csv = csv.replaceAll('"', ' ');
     csv = csv.replace(/\r\n$/, '');
     // console.log(csv);
 
